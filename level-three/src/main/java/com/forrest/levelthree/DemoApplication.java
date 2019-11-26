6f0eb97260e7b446a7bb48540e868f9e7679a7cb
Forrest Knight <forrest@Forrests-MacBook-Pro.local>
2019-11-26 17:27:39 -0500

    added new spring services to instrument

---
+++ level-three/src/main/java/com/forrest/levelthree/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.forrest.levelthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
