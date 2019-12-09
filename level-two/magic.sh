78048c86b619600a1954ddcc91db38c30d1c14e1
Forrest Knight <forrest@Forrests-MacBook-Pro.local>
2019-12-09 14:41:53 -0500

    udpated readme and notes

--- level-two/magic.sh
+++ level-two/magic.sh
@@ -2,5 +2,6 @@ java -javaagent:dd-java-agent.jar \
 -Ddd.agent.host=localhost -Ddd.trace.agent.port=9111 \
 -Ddd.propagation.style.extract=B3 \
 -Ddd.propagation.style.inject=B3 \
+-Ddd.trace.debug=true \
 -Ddd.trace.global.tags='lightstep.service_name:Level 2 Auto Instrumented,lightstep.access_token:qYVqBuZ2QCMskFNzwPC0HjWUx1tADk0XJhZ35YGv276BEWn6tEWSOpgqFu2e7W+jw+HbSaTL3r3x3SO96qNGxhRPh+gjoupgmIYhb1mV' \
 -jar target/level-two-1.jar
