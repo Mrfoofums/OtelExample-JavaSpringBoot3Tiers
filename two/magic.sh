7b404d53109a877abb6f41c7d541ee4975cadaea
Forrest <forrest>
2020-04-21 14:37:16 -0400

    version

--- two/magic.sh
+++ two/magic.sh
@@ -5,5 +5,5 @@ java -Dsa.tracer=lightstep \
 -Dls.accessToken=qYVqBuZ2QCMskFNzwPC0HjWUx1tADk0XJhZ35YGv276BEWn6tEWSOpgqFu2e7W+jw+HbSaTL3r3x3SO96qNGxhRPh+gjoupgmIYhb1mV \
 -Dls.componentName=two \
 -Dls.propagator=b3 \
--javaagent:opentracing-specialagent-1.5.8.jar \
+-javaagent:opentracing-specialagent-1.7.1.jar \
 -jar level-two-1.jar
--- two/src/main/java/com/forrest/leveltwo/Greeting.java
+++ two/src/main/java/com/forrest/leveltwo/Greeting.java
@@ -19,3 +19,4 @@ public class Greeting {
         return content;
     }
 }
+
