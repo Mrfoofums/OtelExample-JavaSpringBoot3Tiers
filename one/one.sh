292afc1df69dfc7a9e208bd885d0c2d25014e360
Forrest <forrest@longviewacquisitions.com>
2020-07-15 16:04:05 -0400

    updated lightstep exporter to 0.3.1 and added explicit env vars for reporting url and port

--- one/one.sh
+++ one/one.sh
@@ -1,9 +1,11 @@
 rm -r ./target
 mvn package
 
-java -Dota.exporter.jar=lightstep-opentelemetry-auto-exporter-0.3.0.jar \
+java -Dota.exporter.jar=lightstep-opentelemetry-auto-exporter-0.3.1.jar \
 -Dota.exporter.lightstep.access.token=qYVqBuZ2QCMskFNzwPC0HjWUx1tADk0XJhZ35YGv276BEWn6tEWSOpgqFu2e7W+jw+HbSaTL3r3x3SO96qNGxhRPh+gjoupgmIYhb1mV \
 -Dota.exporter.lightstep.service.version=forrest \
+-Dota.exporter.lightstep.collector.host=ingest.lightstep.com \
+-Dota.exporter.lightstep.collector.port=443 \
 -Dota.exporter.lightstep.service.name=one \
 -javaagent:opentelemetry-auto-0.3.0.jar \
 -jar ./target/level-one-0.0.1-SNAPSHOT.jar
