ea7c7ab6cef0fffe55b71e7c5573ac02a742bb99
Forrest <forrest@longviewacquisitions.com>
2020-08-12 11:34:18 -0400

    working

--- two/two.sh
+++ two/two.sh
@@ -1,6 +1,8 @@
 rm -r ./target
 mvn package
 
+export LS_ACCESS_TOKEN=6how6J0/2q4Dqxo6VYi6CKXwQJARSmcvydZYNIpti97UAsyjiCbH1aKTp3VoealzQouMtY0FA0xuwvDQhYJFU87fXMngEWngSxDKCSsI
+
 java -javaagent:lightstep-opentelemetry-javaagent-0.7.1.jar \
      -Dls.service.name="Level Two" \
      -jar ./target/level-two-1.jar
