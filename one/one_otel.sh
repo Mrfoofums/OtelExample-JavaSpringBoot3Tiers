c6e26cb11a6153e849c155781ae7ea819c062c45
Forrest <forrest>
2023-02-26 22:18:35 -0500

    fixing launch scripts

--- one/one_otel.sh
+++ one/one_otel.sh
@@ -1,8 +1,6 @@
 rm -r ./target
 ./mvnw package
 
-export OTEL_SERVICE_NAME="Forrest-One"
 
-
-java -javaagent:opentelemetry-javaagent.jar \
-     -jar ./target/level-one-0.0.1-SNAPSHOT.jar   
+export OTEL_SERVICE_NAME="ADT-ONE"
+java -javaagent:opentelemetry-javaagent.jar -jar ./target/level-one-0.0.1-SNAPSHOT.jar   
--- three/three_otel.sh
+++ three/three_otel.sh
@@ -1,9 +1,8 @@
 rm -r ./target
 ./mvnw package
 
-export OTEL_SERVICE_NAME="Forrest-Three"
+export OTEL_SERVICE_NAME="Layer-Three"
 export OTEL_RESOURCE_ATTRIBUTES=service.version=2.2.2.2
-
 
 java -javaagent:opentelemetry-javaagent.jar \
      -jar ./target/level-three-0.0.1-SNAPSHOT.jar
